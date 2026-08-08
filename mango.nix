73db6d8e1025b79bce60e46080ff342d35fd047a
Jeff Apczynski <jeff.apczynski@gmail.com>
2026-08-07 22:15:15 -0700

    use newest mango

--- mango.nix
+++ mango.nix
@@ -1,12 +1,22 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
 {
-  programs.mango.enable = true;
+  disabledModules = [ "programs/wayland/mangowc.nix" ];
+
+  imports = [
+    "${inputs.nixpkgs-unstable}/nixos/modules/programs/wayland/mango.nix"
+  ];
+
+  programs.mango = {
+    enable = true;
+    package = pkgs.unstable.mango;
+  };
 
   environment.systemPackages = with pkgs; [
   ];
 
   programs.waybar = {
     enable = true;
+    systemd.target = "mango.service";
   };
 }
